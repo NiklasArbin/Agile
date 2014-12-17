5f193c63bfce2db06fdfecd1cd970b672acee7bd
Jonas Brandin <jonas.brandin@skandia.se>
2014-12-17 09:49:25 +0100

    Nya klasser

--- src/Agile/Agile.Domain.Model/UserStory.cs
+++ src/Agile/Agile.Domain.Model/UserStory.cs
@@ -9,20 +9,21 @@ namespace Agile.Domain.Model
 
     public interface IAggregateRoot
     {
-        int Id { get; set; }        
+        int Id { get; set; }
     }
     public class UserStory : IDescribed, IAggregateRoot
     {
         public UserStory()
-        { 
+        {
             Tasks = new List<Task>();
+            Impediments = new List<Impediment>();
         }
 
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public List<Task> Tasks { get; set; }
-
-
+        public int SprintId { get; set; }
+        public List<Impediment> Impediments { get; set; }
     }
 }
