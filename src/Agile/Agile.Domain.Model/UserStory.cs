7e98de0ca81a32e204edc77ceb4296ebc53d692c
Jonas Brandin <jonas.brandin@skandia.se>
2014-12-17 09:04:53 +0100

    UserStory and Task

---
+++ src/Agile/Agile.Domain.Model/UserStory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.Domain.Model
+{
+    public class UserStory : IDescribed
+    {
+        public UserStory()
+        { 
+            Tasks = new List<Task>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Task> Tasks { get; set; }
+
+
+    }
+}
