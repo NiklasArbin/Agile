6168d41184c2929d7ec49d5a924e8b471799e544
niklasarbin <niklas.arbin@skandia.se>
2014-12-21 20:34:52 +0100

    added sample menu

---
+++ src/StyrBoard/StyrBoard.Web/App/Ctrl/menuCtrl.js
@@ -0,0 +1,12 @@
+﻿agile.kanbanBoardApp.controller('menuCtrl', function ($scope, $mdSidenav) {
+
+    $scope.menuItems = [
+        {
+            title: 'item 1',
+            link: 'http://google.com'
+        },
+        {
+            title: 'item 2',
+            link: 'http://youtube.com'
+        }];
+});
