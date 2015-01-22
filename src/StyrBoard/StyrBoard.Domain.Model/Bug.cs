2d6a59a4781e20362e1a4bddd2cc4b6537b8553d
skandiasteste <stefan.steding@skandia.se>
2015-01-22 12:14:18 +0100

    Add DisplayId

--- src/StyrBoard/StyrBoard.Domain.Model/Bug.cs
+++ src/StyrBoard/StyrBoard.Domain.Model/Bug.cs
@@ -5,6 +5,7 @@ namespace StyrBoard.Domain.Model
     public class Bug : IDescribed, IAggregateRoot
     {
         public Guid Id { get; set; }
+        public int DisplayId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
--- src/StyrBoard/StyrBoard.Domain.Model/Feature.cs
+++ src/StyrBoard/StyrBoard.Domain.Model/Feature.cs
@@ -11,6 +11,7 @@ namespace StyrBoard.Domain.Model
         }
 
         public Guid Id { get; set; }
+        public int DisplayId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public List<UserStory> UserStories { get; set; }
