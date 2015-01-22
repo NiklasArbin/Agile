dda2f8585f66198f2a3c201e301923fdd76b1389
skandiasteste <stefan.steding@skandia.se>
2015-01-22 11:07:13 +0100

    First draft saving a domain object

---
+++ src/StyrBoard/StyrBoard.Domain.Repository/Model/GuidToIntMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyrBoard.Domain.Repository.Model
+{
+    public class GuidToIntMap
+    {
+        public int Id { get; set; }
+        public Guid GuidId { get; set; }
+    }
+}
---
+++ src/StyrBoard/StyrBoard.Domain.Repository/UserStoryRepository.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Linq;
+using Raven.Client;
+
+using StyrBoard.Domain.Model;
+using StyrBoard.Domain.Repository.Model;
+
+namespace StyrBoard.Domain.Repository
+{
+    public class UserStoryRepository : IRepository<UserStory>
+    {
+        private readonly IDocumentStore _store;
+
+        public UserStoryRepository(IDocumentStore store)
+        {
+            _store = store;
+        }
+
+        public UserStory Get(Guid id)
+        {
+            using (var session = _store.OpenSession())
+            {
+                return session.Query<UserStory>().Single(x => x.Id == id);
+            }
+        }
+
+        public void Save(UserStory item)
+        {
+            using (var session = _store.OpenSession())
+            {
+                if (item.DisplayId < 1)
+                {
+                    var map = new GuidToIntMap { GuidId = item.Id };
+                    session.Store(map);
+                    var id = session.Query<GuidToIntMap>().Single(x => x.GuidId == item.Id).Id;
+                    item.DisplayId = id;
+                }
+
+                session.Store(item);
+                session.SaveChanges();
+            }
+        }
+    }
+}
