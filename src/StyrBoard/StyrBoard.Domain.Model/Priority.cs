f602a0bffa9a6d42cf52c3221e74ec7f4239bed2
Arbin, Niklas <Niklas.Arbin@skandia.se>
2015-02-13 08:55:55 +0100

    separatate Priority.cs

---
+++ src/StyrBoard/StyrBoard.Domain.Model/Priority.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StyrBoard.Domain.Model
+{
+    public class Priority
+    {
+        public Priority()
+        {
+            Items = new List<Guid>();
+        }
+
+        private List<Guid> Items { get; set; }
+
+        public int Add(Guid id)
+        {
+            if(!Items.Contains(id)) Items.Add(id);
+            return Items.IndexOf(id);
+        }
+
+        public int Get(Guid id)
+        {
+            return Items.IndexOf(id);
+        }
+
+        public void Move(Guid id, int index)
+        {
+            if (!Items.Contains(id)) return;
+
+            var oldIndex = Items.IndexOf(id);
+
+            var minIndex = Math.Min(index, oldIndex);
+            var maxIndex = Math.Max(index, oldIndex);
+
+            var changed = new List<Guid>();
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                changed.Add(Items[i]);
+            }
+
+            Items.Remove(id);
+            Items.Insert(index, id);
+
+        }
+        
+    }
+}
